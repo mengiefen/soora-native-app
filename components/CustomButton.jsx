2b81bd04ea4a8d52fd7cfddeb14d5484e1cb0a55
mengiefen <mengiefen@gmail.com>
2022-11-18 23:44:01 +0300

    add custom button

---
+++ components/CustomButton.jsx
@@ -0,0 +1,56 @@
+import { Pressable, StyleSheet, Text } from 'react-native';
+import React from 'react';
+
+const CustomButton = ({
+  children,
+  onPress,
+  color,
+  label,
+  title,
+  variant,
+  style,
+}) => {
+  return (
+    <Pressable
+      onPress={onPress}
+      color={color}
+      style={[styles.button, styles[variant]]}
+      accessibilityLabel={label}
+      sx={style}
+    >
+      <Text style={styles.text}> {title} </Text>
+      {children}
+    </Pressable>
+  );
+};
+
+export default CustomButton;
+
+const styles = StyleSheet.create({
+  button: {
+    paddingHorizontal: 14,
+    paddingVertical: 10,
+    fontSize: 15,
+    fontWeight: 700,
+    backgroundColor: '#0B0B0B',
+    color: '#fff',
+    elevation: 3,
+    borderRadius: 4,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  rounded: {
+    borderRadius: 50,
+    paddingHorizontal: 32,
+    paddingVertical: 12,
+  },
+
+  text: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'semibold',
+  },
+});
