5c44319d71a5e0c8c87caae0f09581addbdf2eb5
mengiefen <mengiefen@gmail.com>
2022-11-18 23:44:29 +0300

    add app store button

---
+++ components/StoreButton.jsx
@@ -0,0 +1,27 @@
+import { StyleSheet, Dimensions, Pressable, Image } from 'react-native';
+import React from 'react';
+
+const { width } = Dimensions.get('window');
+
+const StoreButton = (props) => {
+  return (
+    <Pressable onPress={props.onPress}>
+      <Image source={props.imageUrl} style={styles[props.storeName]} />
+    </Pressable>
+  );
+};
+
+export default StoreButton;
+
+const styles = StyleSheet.create({
+  googlePlay: {
+    width: (134.2 / 390) * width,
+    height: (38.8 / 390) * width,
+    marginRight: 16,
+  },
+
+  appleStore: {
+    width: (118.83 / 390) * width,
+    height: (38.8 / 390) * width,
+  },
+});
