36b32d4f6be3f391621d3b6e20faf915bdd87389
mengiefen <mengiefen@gmail.com>
2022-11-18 23:44:51 +0300

    include landing image

---
+++ components/LandingImage.jsx
@@ -0,0 +1,31 @@
+import { StyleSheet, Image, View, Dimensions } from "react-native";
+import React from "react";
+
+const { width } = Dimensions.get("window");
+
+const LandingImage = (props) => {
+  return (
+    <View style={styles.imageContainer}>
+      <Image source={props.image} style={styles.landingImage} />
+    </View>
+  );
+};
+
+export default LandingImage;
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    marginTop: 30,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 5,
+  },
+
+  landingImage: {
+    width: (350 / 390) * width,
+    height: ((353.9 - 316 + 359.36) / 390) * width,
+    filter: "drop-shadow(0px 2.3304px 11.652px rgba(30, 30, 30, 0.15))",
+  },
+});
