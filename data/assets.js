0bc6a731b4775e871b86c1404bc131250e06f225
mengiefen <mengiefen@gmail.com>
2022-11-18 23:45:37 +0300

    arrange asset file

---
+++ data/assets.js
@@ -0,0 +1,53 @@
+import Facebook from '../assets/images/facebook.png';
+import Twitter from '../assets/images/twitter.png';
+import Instagram from '../assets/images/instagram.png';
+import Tiktok from '../assets/images/tiktok.png';
+import Youtube from '../assets/images/youtube.png';
+
+import GooglePlay from '../assets/images/google-play.png';
+import AppleStore from '../assets/images/apple-store.png';
+import LandingImage2 from '../assets/images/landing-image-2.png';
+import LandingImage1 from '../assets/images/landing-image-1.png';
+
+export const socialMedia = [
+  {
+    name: 'Facebook',
+    image: Facebook,
+    link: 'https://www.facebook.com/yourpage',
+  },
+  {
+    name: 'Twitter',
+    image: Twitter,
+    link: 'https://www.twitter.com/yourpage',
+  },
+  {
+    name: 'Instagram',
+    image: Instagram,
+    link: 'https://www.instagram.com/yourpage',
+  },
+  {
+    name: 'Tiktok',
+    image: Tiktok,
+    link: 'https://www.tiktok.com/yourpage',
+  },
+  {
+    name: 'Youtube',
+    image: Youtube,
+    link: 'https://www.youtube.com/yourpage',
+  },
+];
+
+export const stores = [
+  {
+    name: 'Google Play',
+    image: GooglePlay,
+    link: 'https://play.google.com/store/apps/details?id=com.yourapp',
+  },
+  {
+    name: 'Apple Store',
+    image: AppleStore,
+    link: 'https://apps.apple.com/us/app/yourapp/id000000000',
+  },
+];
+
+export { LandingImage1, LandingImage2, GooglePlay, AppleStore };
