7dd1ecbb855354b1a1c55c5315d64e5fa717f39b
mengiefen <mengiefen@gmail.com>
2022-11-18 23:43:02 +0300

    include footer

---
+++ components/Footer.jsx
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <View style={styles.footerContainer}>
+      <Text style={styles.footerText}>
+        Copyright © 2022 Soora. All rights reserved
+      </Text>
+    </View>
+  );
+};
+
+export default Footer;
+
+const styles = StyleSheet.create({
+  footerContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 15,
+  },
+
+  footerText: {
+    fontSize: 15,
+    color: "#0B0B0B",
+  },
+});
