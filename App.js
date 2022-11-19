64676ed1d52fd5e1cf93b8bac384f499155a5fe0
mengiefen <mengiefen@gmail.com>
2022-11-19 19:12:52 +0300

    load fonts from asset file

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar';-
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
 import Home from './screen/Home';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Inter-Bold': require('./assets/fonts/Inter-Bold.ttf'),
+    'Inter-Medium': require('./assets/fonts/Inter-Medium.ttf'),
+    'Inter-Regular': require('./assets/fonts/Inter-Regular.ttf'),
+    'Inter-SemiBold': require('./assets/fonts/Inter-SemiBold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
     <View style={styles.container}>
+      <Text style={{ fontFamily: 'Inter-Regular', fontSize: 30 }}>
+        Mengistu Fentaw
+      </Text>
       <StatusBar style="auto" />
       <Home />
     </View>
